20abe6d0b4609e05c1d1729810597607a3f1e0a0
utkarshrthr <utkarshrathore529@gmail.com>
2021-11-22 18:27:30 +0530

    initial commit

---
+++ url-generate-service/src/main/java/app/utk/main/UrlGenerateServiceApplication.java
@@ -0,0 +1,13 @@
+package app.utk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlGenerateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlGenerateServiceApplication.class, args);
+	}
+
+}
---
+++ url-redirect-service/src/main/java/app/utk/main/UrlRedirectServiceApplication.java
@@ -0,0 +1,13 @@
+package app.utk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlRedirectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlRedirectServiceApplication.class, args);
+	}
+
+}
